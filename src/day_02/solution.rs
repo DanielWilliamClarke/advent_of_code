eb1b0316907f21edf4d20dd9eb034857d1b06cd6
DanielWilliamClarke <clarkit@gmail.com>
2021-12-06 19:43:01 +0000

    refactoring to remove boilerplate

--- src/day_02/solution.rs
+++ src/day_02/solution.rs
@@ -5,34 +5,14 @@ use std::str::FromStr;
 
 #[derive(Default)]
 struct Trajectory {
-    position: i32,
-    depth: i32,
-    aim: i32,
+    pub position: i32,
+    pub depth: i32,
+    pub aim: i32,
 }
 
 impl Trajectory {
-    pub fn new() -> Trajectory {
-        Trajectory::default()
-    }
-
     pub fn combine(&self) -> i32 {
         self.position * self.depth
-    }
-
-    pub fn update_position(&mut self, distance: &i32) {
-        self.position += distance;
-    }
-
-    pub fn update_depth(&mut self, distance: &i32) {
-        self.depth += distance;
-    }
-
-    pub fn update_aim(&mut self, distance: &i32) {
-        self.aim += distance;
-    }
-
-    pub fn get_aim(&self) -> i32 {
-        self.aim
     }
 }
 
@@ -42,9 +22,9 @@ impl Solution<String, i32> for Day02 {
     fn pt_1(&self, input: &[String]) -> i32 {
         self.parse(input, |mut trajectory, direction, distance| -> Trajectory {
             match direction {
-                "forward" => trajectory.update_position(&distance),
-                "down" => trajectory.update_depth(&distance),
-                "up" => trajectory.update_depth(&-distance),
+                "forward" => trajectory.position += distance,
+                "down" => trajectory.depth += distance,
+                "up" => trajectory.depth -= distance,
                 _ => {}
             };
             trajectory
@@ -55,11 +35,11 @@ impl Solution<String, i32> for Day02 {
         self.parse(input, |mut trajectory, direction, distance| -> Trajectory {
             match direction {
                 "forward" => {
-                    trajectory.update_position(&distance);
-                    trajectory.update_depth(&(trajectory.get_aim() * distance))
+                    trajectory.position += distance;
+                    trajectory.depth += trajectory.aim * distance;
                 }
-                "down" => trajectory.update_aim(&distance),
-                "up" => trajectory.update_aim(&-distance),
+                "down" => trajectory.aim += distance,
+                "up" => trajectory.aim -= distance,
                 _ => {}
             };
             trajectory
@@ -72,13 +52,13 @@ impl Day02 {
         Day02 {}
     }
 
-    fn parse<'a, F>(&self, input: &[String], parser: F) -> i32
+    fn parse<F>(&self, input: &[String], parser: F) -> i32
     where
         F: Fn(Trajectory, &str, i32) -> Trajectory,
     {
         input
             .iter()
-            .fold(Trajectory::new(), move |acc, movement| {
+            .fold(Trajectory::default(), |acc, movement| {
                 let mut split = movement.split(' ');
                 let direction = split.next().unwrap();
                 let distance = <i32 as FromStr>::from_str(split.next().unwrap()).unwrap();
