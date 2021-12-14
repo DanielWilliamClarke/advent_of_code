68f8ee86c5f013aee74b204d7226333186ca92f3
Daniel Clarke <Clarkit@gmail.com>
2021-12-14 16:21:20 +0000

    complete day 7 pt 1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AdventOfCode2021 🎄
 
-[![aoc_2021_badge](https://img.shields.io/badge/Advent%20of%20Code%202021-🌟%2012-blue)](https://adventofcode.com/)
+[![aoc_2021_badge](https://img.shields.io/badge/Advent%20of%20Code%202021-🌟%2013-blue)](https://adventofcode.com/)
 
 ## 🎅 Run
 
--- src/day_07/solution.rs
+++ src/day_07/solution.rs
@@ -4,7 +4,7 @@ pub struct Day07;
 
 impl Solution<String, i32> for Day07 {
     fn pt_1(&self, input: &[String]) -> i32 {
-       0
+        self.optimize(&self.parse(input))
     }
 
     fn pt_2(&self, input: &[String]) -> i32 {
@@ -26,6 +26,20 @@ impl Day07 {
             .collect()
     }
 
+    fn optimize (&self, input: &[i32]) -> i32 {
+        let max = input.iter().max().unwrap();
+        let min = input.iter().min().unwrap();
+
+        (*min..*max)
+            .map(|index| {
+                input
+                    .iter()
+                    .map(|position| index.abs_diff(*position) as i32)
+                    .sum::<i32>()
+            })
+            .min()
+            .unwrap()
+    }
 }
 
 #[cfg(test)]
@@ -37,7 +51,7 @@ mod tests {
         let day = Day07::new();
         let input = day.read_input("src/day_07/input.txt");
         vec![
-            (day.pt_1(&input), 0),
+            (day.pt_1(&input), 347449),
             (day.pt_2(&input), 0),
         ]
         .iter()
