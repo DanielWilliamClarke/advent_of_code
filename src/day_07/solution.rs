a2d82e890b5d39d9ffbe29de8d7061b975543ccc
Daniel Clarke <Clarkit@gmail.com>
2021-12-15 16:22:34 +0000

    refactor day 7

--- src/day_07/solution.rs
+++ src/day_07/solution.rs
@@ -4,15 +4,11 @@ pub struct Day07;
 
 impl Solution<String, u32> for Day07 {
     fn pt_1(&self, input: &[String]) -> u32 {
-        self.optimize(
-            &self.parse(input),
-        |n| n)
+        self.optimize(&self.parse(input), |n| n)
     }
 
     fn pt_2(&self, input: &[String]) -> u32 {
-        self.optimize(
-            &self.parse(input),
-            |n| n * (n + 1) / 2)
+        self.optimize(&self.parse(input), |n| n * (n + 1) / 2)
     }
 }
 
@@ -30,7 +26,7 @@ impl Day07 {
             .collect()
     }
 
-    fn optimize (&self, input: &[u32], cost_predicate: fn(u32) -> u32) -> u32 {
+    fn optimize(&self, input: &[u32], cost_predicate: fn(u32) -> u32) -> u32 {
         let max = input.iter().max().unwrap();
         let min = input.iter().min().unwrap();
 
@@ -38,8 +34,7 @@ impl Day07 {
             .map(|index| {
                 input
                     .iter()
-                    .map(|position|
-                        cost_predicate(index.abs_diff(*position)))
+                    .map(|position| cost_predicate(index.abs_diff(*position)))
                     .sum::<u32>()
             })
             .min()
@@ -55,11 +50,8 @@ mod tests {
     fn solution_is_correct() {
         let day = Day07::new();
         let input = day.read_input("src/day_07/input.txt");
-        vec![
-            (day.pt_1(&input), 347449),
-            (day.pt_2(&input), 98039527),
-        ]
-        .iter()
-        .for_each(|test| assert_eq!(test.0, test.1))
+        vec![(day.pt_1(&input), 347449), (day.pt_2(&input), 98039527)]
+            .iter()
+            .for_each(|test| assert_eq!(test.0, test.1))
     }
 }
