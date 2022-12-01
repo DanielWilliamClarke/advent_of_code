b47fcd314e739563df4fa0b2e92505676c655f8a
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-01 19:27:31 +0000

    day 1 part 1 complete

--- aoc_2022/src/days/day_01.rs
+++ aoc_2022/src/days/day_01.rs
@@ -4,19 +4,35 @@ use crate::utils::solution::Solution;
 pub struct Day01;
 
 impl Solution for Day01 {
-    type Input = i64;
-    type Output = usize;
+    type Input = String;
+    type Output = i32;
 
     fn file_name(&self) -> &'static str {
         "src/days/input/day_1.txt"
     }
 
-    fn pt_1(&self, _: &[Self::Input]) -> Self::Output {
-        0
-    }
+    fn pt_1(&self, input: &[Self::Input]) -> Self::Output {
+        self.parse(&input)
+            .iter()
+            .map(|list| list.iter().sum::<i32>())
+            .max()
+            .unwrap()
+    } 
 
     fn pt_2(&self, _: &[Self::Input]) -> Self::Output {
         0
+    }
+}
+
+impl Day01 {
+    fn parse(&self, input: &[String]) -> Vec<Vec<i32>> {
+        input
+            .split(|line| line.is_empty())
+            .map(|group| group
+                .iter()
+                .map(|line| line.to_string().parse::<i32>().unwrap())
+                .collect::<Vec<i32>>())
+            .collect::<Vec<Vec<i32>>>()
     }
 }
 
@@ -28,6 +44,6 @@ mod tests {
 
     #[test]
     fn solution_is_correct() {
-        Day01{}.validate((0, 0));
+        Day01{}.validate((72511, 0));
     }
 }
