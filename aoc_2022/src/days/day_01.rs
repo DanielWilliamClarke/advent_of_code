2dc0b910bd9835db9e40f7aea8b47c717e5574ba
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-01 22:01:24 +0000

    refactor for generics

--- aoc_2022/src/days/day_01.rs
+++ aoc_2022/src/days/day_01.rs
@@ -1,52 +1,56 @@
 // src/days/day_01.rs
 
-use std::collections::BinaryHeap;
+use std::{collections::BinaryHeap, str::FromStr, iter::Sum};
 use crate::utils::solution::Solution;
+use std::fmt::Debug;
 
 pub struct Day01;
 
 impl Solution for Day01 {
     type Input = String;
-    type Output = i32;
+    type Output = u32;
 
     fn file_name(&self) -> &'static str {
         "src/days/input/day_1.txt"
     }
 
-    fn pt_1(&self, input: &[Self::Input]) -> Self::Output {
-        self.sum_per_elf(input)
-            .iter()
-            .max()
-            .unwrap()
-            .to_owned()
+    fn pt_1(&self, snacks: &[Self::Input]) -> Self::Output {
+        self.sum_per_elf(snacks, 1)
     } 
 
-    fn pt_2(&self, input: &[Self::Input]) -> Self::Output {
-        BinaryHeap::from(self.sum_per_elf(input))
-            .into_sorted_vec()
-            .iter()
-            .rev()
-            .take(3)
-            .sum::<i32>()
+    fn pt_2(&self, snacks: &[Self::Input]) -> Self::Output {
+        self.sum_per_elf(snacks, 3)
     }
 }
 
 impl Day01 {
-    fn parse_elves(&self, input: &[String]) -> Vec<Vec<i32>> {
-        input
-            .split(|line| line.is_empty())
-            .map(|group| group
+    fn parse_elf_snacks<T>(&self, snacks: &[String]) -> Vec<Vec<T>>
+    where 
+        T: FromStr,
+        <T as FromStr>::Err: Debug
+    {
+        snacks
+            .split(|snack| snack.is_empty())
+            .map(|elf| elf
                 .iter()
-                .map(|line| line.to_string().parse::<i32>().unwrap())
-                .collect::<Vec<i32>>())
-            .collect::<Vec<Vec<i32>>>()
+                .map(|snack| snack.to_string().parse::<T>().unwrap())
+                .collect())
+            .collect()
     }
 
-    fn sum_per_elf(&self, input: &[String]) -> Vec<i32> {
-        self.parse_elves(&input)
+    fn sum_per_elf<T>(&self, snacks: &[String], take: usize) -> T
+    where 
+        T: for<'a> Sum<&'a T> + FromStr + Ord,
+        <T as FromStr>::Err: Debug 
+    {
+        self.parse_elf_snacks(snacks)
             .iter()
-            .map(|list| list.iter().sum::<i32>())
-            .collect::<Vec<i32>>()
+            .map(|snack| snack.iter().sum::<T>())
+            .rev()
+            .collect::<BinaryHeap<T>>()
+            .iter()
+            .take(take)
+            .sum()
     }
 }
 
