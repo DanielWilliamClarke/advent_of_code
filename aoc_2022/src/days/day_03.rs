3af6f19290041ea455d7f0ec6a0a791c99e874a7
Daniel <clarkit@gmail.com>
2022-12-03 22:02:32 +0000

    Day 3 part 2

--- aoc_2022/src/days/day_03.rs
+++ aoc_2022/src/days/day_03.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashSet, hash::Hash};
-
 // src/days/day_03/solution.rs
+
+use std::collections::HashSet;
 use crate::utils::solution::Solution;
 
 pub struct Day03;
@@ -17,8 +17,8 @@ impl Solution for Day03 {
         self.prioritize(input)
     }
 
-    fn pt_2(&self, _: &[Self::Input]) -> Self::Output {
-        0
+    fn pt_2(&self, input: &[Self::Input]) -> Self::Output {
+        self.find_badges(input)
     }
 }
 
@@ -29,27 +29,42 @@ impl Day03 {
             let left = self.categorize(compartments.0);
             let right = self.categorize(compartments.1);
 
-            acc + left.intersection(&right)
+            acc + left
+                .intersection(&right)
                 .into_iter()
                 .fold(0, |acc, mismatch| {
-                    acc + self.bucket_priority(mismatch.to_owned())
+                    acc + self.bucket_priority(mismatch)
                 })
         })
     }
 
-    fn categorize(&self, compartment: &str) -> HashSet<char> {
-        compartment
-            .chars()
-            .into_iter()
-            .collect::<HashSet<char>>()
+    fn find_badges(&self, input: &[String]) -> u32 {
+        input.chunks(3).fold(0, |acc, group| {
+            let group = (
+                self.categorize(&group[0]),
+                self.categorize(&group[1]),
+                self.categorize(&group[2])
+            );
+
+            acc + group.0
+                .intersection(&group.1)
+                .cloned()
+                .collect::<HashSet<char>>()
+                .intersection(&group.2)
+                .fold(0, |acc, badge| acc + self.bucket_priority(badge))
+        })
     }
 
-    fn bucket_priority (&self, mismatch: char) -> u32 {
-        let shifted = mismatch as u8 + 1;
+    fn categorize(&self, compartment: &str) -> HashSet<char> {
+        compartment.chars().into_iter().collect::<HashSet<char>>()
+    }
+
+    fn bucket_priority(&self, mismatch: &char) -> u32 {
+        let shifted = *mismatch as u8 + 1;
         (match mismatch {
-            'a'..='z' => shifted - b'a',
-            'A'..='Z' => shifted - b'A' + (b'z' - b'a') + 1,
-            _ => 0
+            'a'..='z' => shifted - b'a', //97 +
+            'A'..='Z' => shifted - b'A' + (b'Z' - b'A') + 1,
+            _ => 0,
         }) as u32
     }
 }
@@ -60,6 +75,6 @@ mod tests {
 
     #[test]
     fn solution_is_correct() {
-        Day03 {}.validate(8185, 0);
+        Day03 {}.validate(8185, 2817);
     }
 }
