307723d4fba8df50758166a9aa0fd8bab353b7f6
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-04 12:37:27 +0000

    day 4 refactor 1

--- aoc_2022/src/days/day_04.rs
+++ aoc_2022/src/days/day_04.rs
@@ -1,6 +1,9 @@
 // src/days/day_04/solution.rs
 use crate::utils::solution::Solution;
 use std::collections::HashSet;
+
+type Zones = (HashSet<usize>, HashSet<usize>);
+
 pub struct Day04;
 
 impl Solution for Day04 {
@@ -12,63 +15,56 @@ impl Solution for Day04 {
     }
 
     fn pt_1(&self, input: &[Self::Input]) -> Self::Output {
-        self.count_full_intersections(input)
+        self.count_overlaps(input, |intersection, zones| {
+            intersection == zones.0 || intersection == zones.1
+        })
     }
 
     fn pt_2(&self, input: &[Self::Input]) -> Self::Output {
-        self.count_all_intersections(input)
+        self.count_overlaps(input, |intersection, _| !intersection.is_empty())
     }
 }
 
 impl Day04 {
-    fn count_full_intersections(&self, input: &[String]) -> usize {
+    fn count_overlaps<F>(&self, input: &[String], resolver: F) -> usize
+    where
+        F: Fn(HashSet<usize>, Zones) -> bool,
+    {
         input
             .iter()
             .filter(|zones| {
                 let zones = self.expand_ranges(zones);
-                let intersection = zones.0.intersection(&zones.1).cloned().collect::<HashSet<usize>>();   
-                intersection == zones.1 || intersection == zones.0
+                let intersection = zones
+                    .0
+                    .intersection(&zones.1)
+                    .cloned()
+                    .collect::<HashSet<usize>>();
+                resolver(intersection, zones)
             })
             .collect::<Vec<&String>>()
             .len()
     }
 
-    fn count_all_intersections(&self, input: &[String]) -> usize {
-        input
-            .iter()
-            .filter(|zones| {
-                let zones = self.expand_ranges(zones);
-                let intersection = zones.0.intersection(&zones.1).cloned().collect::<HashSet<usize>>();   
-                !intersection.is_empty()
-            })
-            .collect::<Vec<&String>>()
-            .len()
-    }
+    fn expand_ranges(&self, zones: &str) -> Zones {
+        let mut zones = zones.split(",").map(|zone| {
+            let range = zone
+                .split("-")
+                .map(|number| number.parse().unwrap())
+                .collect::<Vec<usize>>();
 
-    fn expand_ranges (&self, zones: &str) -> (HashSet<usize>, HashSet<usize>) {
-        let mut zones = zones
-            .split(",")
-                .map(|zone| {
-                    let range = zone
-                        .split("-")
-                        .map(|number| number.parse().unwrap())
-                        .collect::<Vec<usize>>();
+            (range[0]..=range[1]).collect::<HashSet<usize>>()
+        });
 
-                    (range[0]..=range[1]).collect::<HashSet<usize>>()
-                });
-        
-        ( zones.next().unwrap(), zones.next().unwrap() )
+        (zones.next().unwrap(), zones.next().unwrap())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{
-        Day04, utils::validator::validator::Validator
-    };
+    use crate::{utils::validator::validator::Validator, Day04};
 
     #[test]
     fn solution_is_correct() {
-        Day04{}.validate(498, 859);
+        Day04 {}.validate(498, 859);
     }
 }
