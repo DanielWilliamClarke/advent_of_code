20b508fcec81a7a4d654196d77dd14ec6bcb7139
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-08 11:35:55 +0000

    refactor

--- aoc_2022/src/days/day_08.rs
+++ aoc_2022/src/days/day_08.rs
@@ -42,61 +42,16 @@ impl Day08 {
     fn count_visible(&self, grid: &Vec<Vec<usize>>) -> usize {
         let mut visible = 0;
 
-        for i in 1..grid.len() - 1 {
-            for j in 1..grid[i].len() - 1 {
-                let tree = grid[i][j];
+        for index in 1..grid.len() - 1 {
+            for jndex in 1..grid[index].len() - 1 {
+                let tree = grid[index][jndex];
 
-                //look north
-                let mut is_visible = true;
-                for n in 0..i {
-                    if n != i && grid[n][j] >= tree {
-                        is_visible = false;
-                        break;
-                    }
-                }
-                if is_visible {
-                    visible += 1;
-                    continue;
-                }
-
-                // look south
-                let mut is_visible = true;
-                for s in i..grid.len() {
-                    if s != i && grid[s][j] >= tree {
-                        is_visible = false;
-                        break;
-                    }
-                }
-                if is_visible {
-                    visible += 1;
-                    continue;
-                }
-
-                // look west
-                let mut is_visible = true;
-                for w in 0..j {
-                    if w != j && grid[i][w] >= tree {
-                        is_visible = false;
-                        break;
-                    }
-                }
-                if is_visible {
-                    visible += 1;
-                    continue;
-                }
-
-                // look east
-                let mut is_visible = true;
-                for e in j..grid[i].len() {
-                    if e != j && grid[i][e] >= tree {
-                        is_visible = false;
-                        break;
-                    }
-                }
-                if is_visible {
-                    visible += 1;
-                    continue;
-                }
+                visible += (
+                    self.look(0..=index-1, |north| grid[north][jndex] >= tree) || 
+                    self.look(index+1..grid.len(), |south| grid[south][jndex] >= tree) ||
+                    self.look(0..=jndex-1, |west| grid[index][west] >= tree) ||
+                    self.look(jndex+1..grid[index].len(), |east| grid[index][east] >= tree)
+                ) as usize;
             }
         }
 
@@ -106,55 +61,18 @@ impl Day08 {
     fn calculate_scenic_score(&self, grid: &Vec<Vec<usize>>) -> usize {
         let mut highest_score = 0;
 
-        for i in 1..grid.len() - 1 {
-            for j in 1..grid[i].len() - 1 {
-                let tree = grid[i][j];
+        for index in 1..grid.len() - 1 {
+            for jndex in 1..grid[index].len() - 1 {
+                let tree = grid[index][jndex];
 
                 //look north
-                let mut score_north = 0;
-                for n in (0..i).rev() {
-                    if n != i {
-                        score_north += 1;
-                        if grid[n][j] >= tree {
-                            break;
-                        }
-                    }
-                }
+                let score = 
+                    self.score((0..=index-1).rev(), |north| grid[north][jndex] >= tree) *
+                    self.score(index+1..grid.len(), |south| grid[south][jndex] >= tree) *
+                    self.score((0..=jndex-1).rev(), |west| grid[index][west] >= tree) *
+                    self.score(jndex+1..grid[index].len(), |east| grid[index][east] >= tree);
 
-                // look south
-                let mut score_south = 0;
-                for s in i..grid.len() {
-                    if s != i {
-                        score_south += 1;
-                        if grid[s][j] >= tree {
-                            break;
-                        }
-                    }
-                }     
-
-                // look west
-                let mut score_west = 0;
-                for w in (0..j).rev() {
-                    if w != j {
-                        score_west += 1;
-                        if grid[i][w] >= tree {
-                            break;
-                        }
-                    }
-                }
-
-                // look east
-                let mut score_east = 0;
-                for e in j..grid[i].len() {
-                    if e != j {
-                        score_east += 1;
-                        if grid[i][e] >= tree {
-                            break;
-                        }
-                    }
-                }
-
-                let score = score_north * score_south * score_west * score_east;
+                // let score = score_north * score_south * score_west * score_east;
                 if score > highest_score {
                     highest_score = score;
                 }
@@ -162,6 +80,29 @@ impl Day08 {
         }
 
         highest_score
+    }
+
+    fn look<T: Iterator>(&self, range: T, criteria: impl Fn(<T as Iterator>::Item) -> bool) -> bool {
+        for dir in range { 
+            if criteria(dir) {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn score<T: Iterator>(&self, range: T, criteria: impl Fn(<T as Iterator>::Item) -> bool) -> usize {
+        let mut score = 0;
+
+        for dir in range { 
+            score += 1;
+            if criteria(dir) {
+                break;  
+            }
+        }
+
+        score
     }
 }
 
