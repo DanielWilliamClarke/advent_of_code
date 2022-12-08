a2064207cb15cfea491a2b093057798a50b07216
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-08 10:01:18 +0000

    day 8 complete

--- aoc_2022/src/days/day_08.rs
+++ aoc_2022/src/days/day_08.rs
@@ -17,8 +17,9 @@ impl Solution for Day08 {
         self.count_perimeter(&grid) + self.count_visible(&grid)
     }
 
-    fn pt_2(&self, _: &[Self::Input]) -> Self::Output {
-        0
+    fn pt_2(&self, input: &[Self::Input]) -> Self::Output {
+        let grid = self.parse(input);
+        self.calculate_scenic_score(&grid)
     }
 }
 
@@ -101,6 +102,67 @@ impl Day08 {
 
         visible
     }
+
+    fn calculate_scenic_score(&self, grid: &Vec<Vec<usize>>) -> usize {
+        let mut highest_score = 0;
+
+        for i in 1..grid.len() - 1 {
+            for j in 1..grid[i].len() - 1 {
+                let tree = grid[i][j];
+
+                //look north
+                let mut score_north = 0;
+                for n in (0..i).rev() {
+                    if n != i {
+                        score_north += 1;
+                        if grid[n][j] >= tree {
+                            break;
+                        }
+                    }
+                }
+
+                // look south
+                let mut score_south = 0;
+                for s in i..grid.len() {
+                    if s != i {
+                        score_south += 1;
+                        if grid[s][j] >= tree {
+                            break;
+                        }
+                    }
+                }     
+
+                // look west
+                let mut score_west = 0;
+                for w in (0..j).rev() {
+                    if w != j {
+                        score_west += 1;
+                        if grid[i][w] >= tree {
+                            break;
+                        }
+                    }
+                }
+
+                // look east
+                let mut score_east = 0;
+                for e in j..grid[i].len() {
+                    if e != j {
+                        score_east += 1;
+                        if grid[i][e] >= tree {
+                            break;
+                        }
+                    }
+                }
+
+                let score = score_north * score_south * score_west * score_east;
+                if score > highest_score {
+                    highest_score = score;
+                }
+            }
+        }
+
+        highest_score
+    }
 }
 
 #[cfg(test)]
@@ -109,6 +171,6 @@ mod tests {
 
     #[test]
     fn solution_is_correct() {
-        Day08 {}.validate(1538, 0);
+        Day08 {}.validate(1538, 496125);
     }
 }
