9afb75dde08633b58b3a0ed426b5519c0841ed42
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-10 14:14:49 +0000

    day 10 part 1

--- aoc_2022/src/days/day_10.rs
+++ aoc_2022/src/days/day_10.rs
@@ -4,19 +4,56 @@ use crate::utils::solution::Solution;
 pub struct Day10;
 
 impl Solution for Day10 {
-    type Input = i32;
-    type Output = usize;
+    type Input = String;
+    type Output = isize;
 
     fn file_name(&self) -> &'static str {
         "src/days/input/day_10.txt"
     }
 
-    fn pt_1(&self, _: &[Self::Input]) -> Self::Output {
-        0
+    fn pt_1(&self, input: &[Self::Input]) -> Self::Output {
+        let stream = self.parse(input, 2);
+        self.extract(stream, vec![20, 60, 100, 140, 180, 220])
     }
 
     fn pt_2(&self, _: &[Self::Input]) -> Self::Output {
         0
+    }
+}
+
+impl Day10 {
+    fn parse(&self, input: &[String], cycles: usize) -> Vec<isize> {
+        input
+            .iter()
+            .fold(vec![1 as isize], |mut acc, command| {
+                let mut command = command.split_whitespace();
+
+                match command.next().unwrap() {
+                    "addx" => {
+                         let value = command.next().unwrap().parse::<isize>().unwrap();
+                         (1..=cycles).fold(acc, |mut acc, index| {
+                            let cycle_value = match index {
+                                2 =>  acc.last().unwrap().clone() + value, 
+                                _ => acc.last().unwrap().clone()
+                            };
+                            acc.push(cycle_value);
+                            acc
+                         })
+                    }
+                    _ => {
+                        acc.push(acc.last().unwrap().clone());
+                        acc
+                    }
+                }
+            })
+    }
+
+    fn extract (&self, stream: Vec<isize>, sampling: Vec<usize>) -> isize {
+        sampling
+            .iter()
+            .fold(0, |acc, sample_index| {
+                acc + (stream.get(sample_index  - 1).unwrap() * *sample_index as isize)
+            })
     }
 }
 
