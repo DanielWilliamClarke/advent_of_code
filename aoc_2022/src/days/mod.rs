49e54ad6b3e021923752da2fe35e87b0ff4bdc4a
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-12-12 14:04:42 +0000

    refactor

--- aoc_2022/src/days/mod.rs
+++ aoc_2022/src/days/mod.rs
@@ -2,26 +2,37 @@
 
 mod day_00;
 pub use day_00::Day00;
+
 mod day_01;
 pub use day_01::Day01;
+
 mod day_02;
 pub use day_02::Day02;
+
 mod day_03;
 pub use day_03::Day03;
+
 mod day_04;
 pub use day_04::Day04;
+
 mod day_05;
 pub use day_05::Day05;
+
 mod day_06;
 pub use day_06::Day06;
+
 mod day_07;
 pub use day_07::Day07;
+
 mod day_08;
 pub use day_08::Day08;
+
 mod day_09;
 pub use day_09::Day09;
+
 mod day_10;
 pub use day_10::Day10;
+
 mod day_11;
 pub use day_11::Day11;
 
