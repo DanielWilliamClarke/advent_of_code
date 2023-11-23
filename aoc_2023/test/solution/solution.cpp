54d89a56b19d44cc236ef15d61e904e972ea236d
Daniel William Clarke <dan.clarke@loveholidays.com>
2023-11-23 23:56:49 +0000

    fix tests

--- aoc_2023/test/solution/solution.cpp
+++ aoc_2023/test/solution/solution.cpp
@@ -6,8 +6,8 @@ class TestReader : public Solution<int>
 {
 public:
     constexpr std::string filename() const override { return "test/solution/input.txt"; }
-    int part1(std::vector<std::string>) const override { return 0; }
-    int part2(std::vector<std::string>) const override { return 0; }
+    int part1(const std::vector<std::string>&) const override { return 0; }
+    int part2(const std::vector<std::string>&) const override { return 0; }
 };
 
 TEST(Solution, CanReadAFileToVector) 
