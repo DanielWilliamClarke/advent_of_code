09d6f06e2bcd5b4a3374253bb78ee6f942d24648
Daniel William Clarke <dan.clarke@loveholidays.com>
2023-11-25 23:56:45 +0000

    remove runner

--- aoc_2023/main/main.cpp
+++ aoc_2023/main/main.cpp
@@ -2,14 +2,17 @@
 #include <iomanip>
 #include <memory>
 
+#include <range/v3/all.hpp>
+
 #include "main/solution/printer.h"
-#include "main/solution/runner.h"
 
 #include "main/days/00/day_00.h"
 
 int main () 
 {
-    run({
+    auto days = {
         std::make_shared<Day00>()
-    });
+    };
+
+    ranges::for_each(days, [=] (auto day) { day->print(); });
 }
