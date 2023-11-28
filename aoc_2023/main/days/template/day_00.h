eeb3d6eef4d31208df76000dccae5a02d5d4950c
Daniel William Clarke <dan.clarke@loveholidays.com>
2023-11-28 14:29:23 +0000

    update template

--- aoc_2023/main/days/template/day_00.h
+++ aoc_2023/main/days/template/day_00.h
@@ -9,9 +9,9 @@ class Day00 : public Day<std::string, int>
 public:
     virtual ~Day00() = default;
 
-    constexpr std::string filename() const override;
-    int part1(const std::vector<std::string>&) const override;
-    int part2(const std::vector<std::string>&) const override;
+    [[nodiscard]] constexpr std::string filename() const override;
+    [[nodiscard]] int part1(const std::vector<std::string>&) const override;
+    [[nodiscard]] int part2(const std::vector<std::string>&) const override;
 };
 
 #endif // DAY_00_H
