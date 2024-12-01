71ccbcaf8bdadfe5d97218477b53ae56c376d67e
dan.clarke <clarkit@gmail.com>
2024-12-01 16:38:25 +0000

    day 1 part 2 + refactor

---
+++ aoc_2024/day_01/day_01.lua
@@ -0,0 +1,69 @@
+package.path = package.path .. ";../utils/?.lua"
+
+local test = require "simple_test"
+local read_file = require "read_file"
+
+local function parseLine(line)
+    local locationIds = {}
+    for word in string.gmatch(line, "%S+") do
+        table.insert(locationIds, word)
+    end
+    return {
+        list1 = tonumber(locationIds[1]),
+        list2 = tonumber(locationIds[2])
+    }
+end
+
+local function part1()
+    local lines = read_file.parse("input.txt", parseLine)
+
+    -- Create lists and sort ASC
+    local list1, list2 = {}, {}
+    for _, line in pairs(lines) do
+        table.insert(list1, line.list1)
+        table.insert(list2, line.list2)
+    end
+    table.sort(list1)
+    table.sort(list2)
+
+    -- Calculate distance
+    local distance = 0
+    for i = 1, #lines do
+        distance = distance + math.abs(list1[i] - list2[i])
+    end
+
+    print("Part 1: " .. distance)
+    return distance
+end
+
+local function part2()
+    local lines = read_file.parse("input.txt", parseLine)
+
+    local count_table = {}
+    for _, line in pairs(lines) do
+        count_table[line.list2] = (count_table[line.list2] or 0) + 1
+    end
+
+    local similarity_score = 0
+    for _, line in pairs(lines) do
+        local multiplier = (count_table[line.list1] or 0)
+        similarity_score = similarity_score + (line.list1 * multiplier)
+    end
+
+    print("Part 2: " .. similarity_score)
+    return similarity_score
+end
+
+test(
+    "Part 1",
+    function(a)
+        a.ok(part1() == 2000468, "Part 1 solution incorrect!")
+    end
+)
+
+test(
+    "Part 2",
+    function(a)
+        a.ok(part2() == 18567089, "Part 2 solution incorrect!")
+    end
+)
