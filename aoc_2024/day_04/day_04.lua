7e677c2ad074064fe6d976247b5f7c2634641238
dan.clarke <clarkit@gmail.com>
2024-12-04 13:52:20 +0000

    day 04 part 1 start

--- aoc_2024/day_04/day_04.lua
+++ aoc_2024/day_04/day_04.lua
@@ -17,7 +17,34 @@ local function part1()
 
     for i = 1, #lines do
         for j = 1, #lines[i] do
-            print(lines[i][j])
+
+            if (lines[i][j] == 'X') then
+
+                for ni = i - 1, 3 do
+                    for nj = j - 1, 3 do
+                        if (
+                            ni > 0 and ni <= #lines and
+                            nj > 0 and nj <= #lines[j] and
+                            lines[ni][nj] == 'M'
+                        ) then
+                            local dir = { i - ni, j - nj }
+
+                            print(ni .. "," .. nj)
+                            print(dir[1] .. "," .. dir[2])
+                        end
+
+                    end
+               end
+
+                -- search neighbours for an 'M'
+                -- bounds check
+                -- on finding an 'M' search for an 'A' and 'S' in its direction
+                -- if found count XMAS
+                -- keep searching until no more 'M's can be found in neighbours
+                -- move on to the next X
+
+            end
+
         end
     end
 
