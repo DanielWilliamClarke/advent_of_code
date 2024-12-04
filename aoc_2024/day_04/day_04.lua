d6f37d011d3295d0e6633be8c907779549570b68
dan.clarke <clarkit@gmail.com>
2024-12-04 13:18:11 +0000

    day 4 skeleton

---
+++ aoc_2024/day_04/day_04.lua
@@ -0,0 +1,29 @@
+package.path = package.path .. ";../utils/?.lua"
+
+local test = require "simple_test"
+local read_file = require "read_file"
+local timing = require "timing"
+
+local function part1()
+    print(0)
+    return 0
+end
+
+local function part2()
+   print(0)
+       return 0
+end
+
+test(
+    "🎅 Part 1",
+    function(a)
+        a.ok(timing.measure(part1) == 0, "Part 1 solution incorrect!")
+    end
+)
+
+test(
+    "🎄 Part 2",
+    function(a)
+        a.ok(timing.measure(part2) == 0, "Part 2 solution incorrect!")
+    end
+)
