3c8a406509f5181c3d333a328c6fd9190e9996ed
dan.clarke <clarkit@gmail.com>
2024-12-04 10:46:11 +0000

    day 3 part 2 refactor

--- aoc_2024/day_03/day_03.lua
+++ aoc_2024/day_03/day_03.lua
@@ -4,6 +4,10 @@ local test = require "simple_test"
 local read_file = require "read_file"
 local timing = require "timing"
 
+local MUL_PATTERN = "(mul%((%d%d?%d?),(%d%d?%d?)%))";
+local DONT_PATTERN = "don't%(%)"
+local DO_PATTERN = "do%(%)"
+
 local function part1()
     local program =
         read_file.parse(
@@ -15,8 +19,8 @@ local function part1()
 
     local result = 0
     for _, line in ipairs(program) do
-        for match, instruction_a, instruction_b in string.gmatch(line, "(mul%((%d%d?%d?),(%d%d?%d?)%))") do
-            result = result + (instruction_a * instruction_b)
+        for match, a, b in string.gmatch(line, MUL_PATTERN) do
+            result = result + (a * b)
         end
     end
 
@@ -34,40 +38,32 @@ local function part2()
     )
 
     local result = 0
-    local should_do = true
-
+    local enabled = true
     for _, line in ipairs(program) do
-        local current_line = line
-        local next_mul_pos = 0
-        local next_do_pos = 0
-
+        local next_pos = 0
         repeat
-            if should_do then
-                next_mul_pos = string.find(current_line, "(mul%((%d%d?%d?),(%d%d?%d?)%))") or math.huge
-                if next_mul_pos == math.huge then
+            if enabled then
+                next_pos = string.find(line, MUL_PATTERN)
+                if next_pos == nil then goto continue end
+
+                local next_dont = string.find(line, DONT_PATTERN)
+                if next_dont ~= nill and next_dont < next_pos then
+                    enabled = false
                     goto continue
                 end
 
-                local next_dont = string.find(current_line, "don't%(%)") or math.huge
-                if next_dont < next_mul_pos then
-                    should_do = false
-                    goto continue
-                end
-
-                local match, instruction_a, instruction_b = string.match(current_line, "(mul%((%d%d?%d?),(%d%d?%d?)%))")
-
-                result = result + (instruction_a * instruction_b)
-                current_line = string.sub(current_line, next_mul_pos + #match)
+                local match, a, b = string.match(line, MUL_PATTERN)
+                result = result + (a * b)
+                line = string.sub(line, next_pos + #match)
             else
-                next_do_pos = string.find(current_line, "do%(%)") or math.huge
-                if next_do_pos ~= math.huge then
-                    current_line = string.sub(current_line, next_do_pos)
-                    should_do = true
-                end
+                next_pos = string.find(line, DO_PATTERN)
+                if next_pos == nil then goto continue end
+                line = string.sub(line, next_pos)
+                enabled = true
             end
 
             ::continue::
-        until (next_mul_pos == math.huge or next_do_pos == math.huge)
+        until (next_pos == nil)
     end
 
     print(result)
