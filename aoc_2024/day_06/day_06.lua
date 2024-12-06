735bc21dc9140b283d5bcfc3a62850adeb3c5559
dan.clarke <clarkit@gmail.com>
2024-12-06 23:32:07 +0000

    day 06 part 1 with visual

---
+++ aoc_2024/day_06/day_06.lua
@@ -0,0 +1,119 @@
+package.path = package.path .. ";../utils/?.lua"
+
+local test = require "simple_test"
+local read_file = require "read_file"
+local timing = require "timing"
+
+local function parseLine(line)
+    local row = {}
+    for char in string.gmatch(line, ".") do
+        table.insert(
+            row,
+            {
+                char = char,
+                visited = false
+            }
+        )
+    end
+    return row
+end
+
+local function within_bounds(grid, coord)
+    return coord[1] >= 1 and coord[1] <= #grid and coord[2] >= 1 and coord[2] <= #grid[1]
+end
+
+local function rotate_vector(coord, clockwise)
+    -- coord is { y , x }
+    if clockwise then
+        return { coord[2], -coord[1] } -- x, -y
+    else
+        return { -coord[2], coord[1] } -- -x, y
+    end
+end
+
+local function print_grid (grid)
+     for y = 1, #grid do
+        local row = {}
+
+        for x = 1, #grid[1] do
+            if grid[y][x].visited == true then
+                table.insert(row, "X")
+            else
+                table.insert(row,  grid[y][x].char)
+            end
+        end
+
+        print(table.concat(row, ""))
+    end
+end
+
+local function part1()
+    local grid = read_file.parse("input.txt", parseLine)
+
+    -- find guard
+    local guard = {0, 0}
+    for y = 1, #grid do
+        for x = 1, #grid[1] do
+            if grid[y][x].char == "^" then
+                guard = {y, x}
+                goto found
+            end
+        end
+    end
+    ::found::
+
+    local dir = {-1, 0}
+    local exited = false
+
+    -- Just in case the guard doesnt revisit its origin
+    -- visit it to start with
+    local visited_count = 1
+    grid[guard[1]][guard[2]].visited = true
+
+    repeat
+        local np = {
+            guard[1] + dir[1], -- y
+            guard[2] + dir[2] -- x
+        }
+
+        if not within_bounds(grid, np) then
+            exited = true
+        else
+            -- print(np[1]..","..np[2])
+            local char = grid[np[1]][np[2]].char
+            if char == "." or char == "^" then
+                guard = np
+                if not grid[np[1]][np[2]].visited then
+                    visited_count = visited_count + 1
+                    grid[np[1]][np[2]].visited = true
+                end
+            elseif char == "#" then
+                dir = rotate_vector(dir, true)
+            end
+        end
+    until (exited == true)
+
+    -- print_grid(grid)
+
+    print(visited_count)
+    return visited_count
+end
+
+local function part2()
+    print(0)
+    return 0
+end
+
+test(
+    "🎅 Part 1",
+    function(a)
+        a.ok(timing.measure(part1) == 4988, "Part 1 solution incorrect!")
+    end
+)
+
+test(
+    "🎄 Part 2",
+    function(a)
+        a.ok(timing.measure(part2) == 0, "Part 2 solution incorrect!")
+    end
+)
