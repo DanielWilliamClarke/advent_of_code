ec8f9be9d706d0cd3938b1e77a08b5a974c08d8d
dan.clarke <clarkit@gmail.com>
2024-12-09 00:17:35 +0000

    day 8 part 2 solved

--- aoc_2024/day_08/day_08.lua
+++ aoc_2024/day_08/day_08.lua
@@ -19,35 +19,38 @@ local function extract_nodes(grid)
             local frequency = grid[y][x]
             if frequency ~= "." then
                 if grouped_nodes[frequency] == nil then
-                    grouped_nodes[frequency] = { }
+                    grouped_nodes[frequency] = {}
                 end
 
-                table.insert(grouped_nodes[frequency], {
-                    frequency=frequency,
-                    coord={ y=y, x=x }
-                })
+                table.insert(
+                    grouped_nodes[frequency],
+                    {
+                        frequency = frequency,
+                        coord = {y = y, x = x}
+                    }
+                )
             end
         end
     end
     return grouped_nodes
 end
 
-local function pair_up_nodes (nodes)
+local function pair_up_nodes(nodes)
     local paired_nodes = {}
 
-    for f,frequency in pairs(nodes) do
+    for f, frequency in pairs(nodes) do
         if paired_nodes[f] == nil then
-            paired_nodes[f] = { }
+            paired_nodes[f] = {}
         end
 
-        for _,left in ipairs(frequency) do
-            for _,right in ipairs(frequency) do
+        for _, left in ipairs(frequency) do
+            for _, right in ipairs(frequency) do
                 if left ~= right then
-                    local pair = { left, right }
+                    local pair = {left, right}
 
                     -- check if pair already present in pairs
                     local duplicate = false
-                    for _,p in ipairs(paired_nodes[f]) do
+                    for _, p in ipairs(paired_nodes[f]) do
                         -- if left or right pair are present either l->r or r-> l reject it
                         if (p[1] == left and p[2] == right) or (p[1] == right and p[2] == left) then
                             duplicate = true
@@ -65,19 +68,19 @@ local function pair_up_nodes (nodes)
     return paired_nodes
 end
 
-local function print_nodes (nodes)
-    for i,f in pairs(nodes) do
+local function print_nodes(nodes)
+    for i, f in pairs(nodes) do
         print("FREQUENCY " .. i)
-        for _,n in ipairs(f) do
+        for _, n in ipairs(f) do
             print("(" .. n.coord.x .. ", " .. n.coord.y .. ")")
         end
     end
 end
 
-local function print_pairs (nodes)
-    for i,f in pairs(nodes) do
+local function print_pairs(nodes)
+    for i, f in pairs(nodes) do
         print("FREQUENCY " .. i)
-        for _,n in ipairs(f) do
+        for _, n in ipairs(f) do
             print("(" .. n[1].coord.x .. ", " .. n[1].coord.y .. "), (" .. n[2].coord.x .. ", " .. n[2].coord.y .. ")")
         end
     end
@@ -90,44 +93,74 @@ end
 local function get_direction(a, b)
     local dx = b.x - a.x
     local dy = b.y - a.y
-    return { x=dx, y=dy }
+    return {x = dx, y = dy}
 end
 
-local function is_new_antinode (antinodes, antinode)
-    for _,n in ipairs(antinodes) do
+local function find_antinode(antinodes, antinode)
+    for _, n in ipairs(antinodes) do
         if n.x == antinode.x and n.y == antinode.y then
-            return false
+            return antinode
         end
     end
 
-    return true
+    return nill
 end
 
-local function find_antinodes (grid, paired_nodes)
-    local antinodes = {}
-    for _,frequency in pairs(paired_nodes) do
-        for _,p in ipairs(frequency) do
-            local direction = get_direction(p[1].coord, p[2].coord)
+local function print_antinodes_on_grid(grid, antinodes)
+     for y = 1, #grid do
+        local row = {}
+        for x = 1, #grid[1] do
+            if (find_antinode(antinodes, {x=x, y=y}) == nil) then
+                --table.insert(row, grid[y][x])
+                table.insert(row, ".")
+            else
+               table.insert(row, "#")
+            end
+        end
+        print(table.concat(row, ""))
+    end
+end
 
+local function find_antinodes(grid, paired_nodes, repeat_nodes)
+    local antinodes = {}
+    for _, frequency in pairs(paired_nodes) do
+        for _, p in ipairs(frequency) do
+            local direction = get_direction(p[1].coord, p[2].coord)
             -- print("DIRECTION: x: " .. direction.x .. ", y: " .. direction.y)
 
-            local first_antinode = {
-                x=p[2].coord.x + direction.x,
-                y=p[2].coord.y + direction.y
-            }
-            if within_bounds(grid, first_antinode) and is_new_antinode(antinodes, first_antinode) then
+            local first_antinode = p[2].coord
+            if repeat_nodes and within_bounds(grid, first_antinode) and find_antinode(antinodes, first_antinode) == nil then
                 -- print("Adding first antinode: x: " .. first_antinode.x .. ", y: " .. first_antinode.y)
                 table.insert(antinodes, first_antinode)
             end
+            repeat
+                first_antinode = {
+                    x = first_antinode.x + direction.x,
+                    y = first_antinode.y + direction.y
+                }
+                -- print("Checking first antinode: x: " .. first_antinode.x .. ", y: " .. first_antinode.y)
+                if within_bounds(grid, first_antinode) and find_antinode(antinodes, first_antinode) == nil then
+                    -- print("Adding first antinode: x: " .. first_antinode.x .. ", y: " .. first_antinode.y)
+                    table.insert(antinodes, first_antinode)
+                end
+            until not repeat_nodes or not within_bounds(grid, first_antinode)
 
-            local second_antinode = {
-                x=p[1].coord.x + -direction.x,
-                y=p[1].coord.y + -direction.y
-            }
-            if within_bounds(grid, second_antinode) and is_new_antinode(antinodes, second_antinode) then
-                -- print("Adding second antinode: x: " .. second_antinode.x .. ", y: " .. second_antinode.y)
+            local second_antinode = p[1].coord
+            if repeat_nodes and within_bounds(grid, second_antinode) and find_antinode(antinodes, second_antinode) == nil then
+                -- print("Adding first antinode: x: " .. first_antinode.x .. ", y: " .. first_antinode.y)
                 table.insert(antinodes, second_antinode)
             end
+            repeat
+                second_antinode = {
+                    x = second_antinode.x + -direction.x,
+                    y = second_antinode.y + -direction.y
+                }
+                 -- print("Checking second antinode: x: " .. second_antinode.x .. ", y: " .. second_antinode.y)
+                if within_bounds(grid, second_antinode) and find_antinode(antinodes, second_antinode) == nil then
+                    -- print("Adding second antinode: x: " .. second_antinode.x .. ", y: " .. second_antinode.y)
+                    table.insert(antinodes, second_antinode)
+                end
+            until not repeat_nodes or not within_bounds(grid, second_antinode)
         end
     end
     return antinodes
@@ -142,16 +175,29 @@ local function part1()
     local paired_nodes = pair_up_nodes(nodes)
     -- print_pairs(paired_nodes)
 
-    local antinodes = find_antinodes(grid, paired_nodes)
-    local count = #antinodes
+    local antinodes = find_antinodes(grid, paired_nodes, false)
+    -- print_antinodes_on_grid(grid, antinodes)
 
+    local count = #antinodes
     print(count)
     return count
 end
 
 local function part2()
-    print(0)
-    return 0
+    local grid = read_file.parse("input.txt", parseLine)
+
+    local nodes = extract_nodes(grid)
+    -- print_nodes(nodes)
+
+    local paired_nodes = pair_up_nodes(nodes)
+    -- print_pairs(paired_nodes)
+
+    local antinodes = find_antinodes(grid, paired_nodes, true)
+    -- print_antinodes_on_grid(grid, antinodes)
+
+    local count = #antinodes
+    print(count)
+    return count
 end
 
 test(
@@ -164,6 +210,6 @@ test(
 test(
     "🎄 Part 2",
     function(a)
-        a.ok(timing.measure(part2) == 0, "Part 2 solution incorrect!")
+        a.ok(timing.measure(part2) == 1017, "Part 2 solution incorrect!")
     end
 )
