2ef09f1a465e041adbbea8e1af484f70cb2555ee
dan.clarke <clarkit@gmail.com>
2024-12-19 01:46:54 +0000

    day 12 part 2 works on example not input

--- aoc_2024/day_12/day_12.lua
+++ aoc_2024/day_12/day_12.lua
@@ -93,6 +93,101 @@ local function calculate_perimeter(region)
     return perimeter
 end
 
+local function rotate_vector(coord, clockwise)
+    -- coord is { y , x }
+    if clockwise then
+        return {coord[2], -coord[1]} -- x, -y
+    else
+        return {-coord[2], coord[1]} -- -x, y
+    end
+end
+
+
+local function count_sides(region)
+    if #region == 1 then 
+        print("Region " .. region[1].garden .. " sides: " .. 4)
+        return 4 
+    end
+
+    local sides = 1
+    local regionSet = {}
+
+    for _,garden in ipairs(region) do
+        regionSet[garden.coord[1] .. "," .. garden.coord[2]] = {
+            visited=false
+        }
+    end
+
+    local origin = region[1].coord
+
+    local current = {
+        origin[1],
+        origin[2]
+    }
+    local dir_normal = { -1, 0 } -- up
+    local dir_forward = rotate_vector(dir_normal, true) -- right
+    repeat 
+        -- print("Region " .. region[1].garden .. " current: " .. current[1] .. "," .. current[2] .. " direction: " .. dir_forward[1] .. "," .. dir_forward[2] .. " sides " .. sides)
+
+        -- lets have two vectors 90 degrees from each other 
+        -- if going right -> then we should have another perpendicular
+        -- keep checking each square
+        local look_normal = {  -- look perpendicular to direction of travel
+            current[1] + dir_normal[1],
+            current[2] + dir_normal[2]
+        }
+        local look_forward = {  -- look in direction of travel
+            current[1] + dir_forward[1],
+            current[2] + dir_forward[2]
+        }
+
+        local rotated = false
+        if regionSet[look_normal[1] .. "," .. look_normal[2]] then
+            -- increment sides now we need to change direction
+            -- rotate anti-clockwise
+            dir_normal = rotate_vector(dir_normal)
+            dir_forward = rotate_vector(dir_forward)
+            rotated = true
+            look_forward = {  -- look in direction of travel
+                current[1] + dir_forward[1],
+                current[2] + dir_forward[2]
+            }
+        elseif not regionSet[look_forward[1] .. "," .. look_forward[2]] then
+            -- increment sides now we need to change direction
+            -- rotate clockwise 
+            dir_normal = rotate_vector(dir_normal, true)
+            dir_forward = rotate_vector(dir_forward, true)
+            rotated = true
+            look_forward = {  -- look in direction of travel
+                current[1] + dir_forward[1],
+                current[2] + dir_forward[2]
+            }
+        end
+        
+        if regionSet[look_forward[1] .. "," .. look_forward[2]]then
+            -- move in direction of travel fine
+            current = look_forward
+        end
+
+        if rotated then 
+            sides = sides + 1
+        end 
+    until (
+        (current[1] == origin[1] and current[2] == origin[2]) -- and
+        -- (dir_forward[1] == 1 and dir_forward[2] == 0)
+        -- back at origin and direction is going up
+    )
+
+    -- cludge
+    if dir_forward[1] == 0 and dir_forward[2] == -1 then
+        sides = sides + 1
+    end
+
+    print("Region " .. region[1].garden .. " sides: " .. sides .. "end dir" .. dir_forward[1] .. "," .. dir_forward[2])
+
+    return sides
+end
+
 local function part1()
     local grid = read_file.parse("input.txt", parse_line)
     -- print_grid(grid)
@@ -124,8 +219,33 @@ local function part1()
 end
 
 local function part2()
-    print(0)
-    return 0
+    local grid = read_file.parse("example.txt", parse_line)
+    -- print_grid(grid)
+
+    local regions = {}
+    for y = 1, #grid do
+        for x = 1, #grid[1] do
+            if not grid[y][x].visited then
+                local garden = grid[y][x].garden;
+                local coord = {y, x}
+                local region = {
+                    {
+                        garden=garden,
+                        coord=coord
+                    }
+                }
+
+                table.insert(regions, search(grid, coord, garden, region))
+            end
+        end
+    end
+
+    local price = 0
+    for _,region in ipairs(regions) do
+        -- print("Garden " .. region[1].garden .. " Size: " .. #region)
+        price = price + count_sides(region) * #region
+    end
+    return price
 end
 
 test(
@@ -138,6 +258,6 @@ test(
 test(
     "🎄 Part 2",
     function(a)
-        a.ok(timing.measure(part2) == 0, "Part 2 solution incorrect!")
+        a.ok(timing.measure(part2) == 1206, "Part 2 solution incorrect!")
     end
 )
