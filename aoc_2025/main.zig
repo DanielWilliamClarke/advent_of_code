e462d8de39392e791ed7e354176dc64c2d43cca4
dan.clarke <clarkit@gmail.com>
2025-10-23 12:23:37 +0100

    adding tests

--- aoc_2025/main.zig
+++ aoc_2025/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
+}
+
+test "always succeeds" {
+    try expect(true);
 }
