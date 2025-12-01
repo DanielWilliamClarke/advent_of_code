a8ebc8f27e9df6a78579713c55069788caa77a5e
Daniel <clarkit@gmail.com>
2025-12-01 13:38:06 +0000

    Refactor turn functions for clarity: rename turnSafeDial to findPassword and update related logic

--- aoc_2025/src/days/day01.zig
+++ aoc_2025/src/days/day01.zig
@@ -7,6 +7,8 @@ pub const input_path = "inputs/day01.txt";
 pub const example_path = "inputs/day01_example.txt";
 
 const MAX = 99;
+const MAX_INC = MAX + 1;
+const START = 50;
 
 const Turn = struct {
     dir: u8,
@@ -44,14 +46,13 @@ pub fn parse(alloc: std.mem.Allocator, lines: []const []const u8) ![]i64 {
     return turns.toOwnedSlice(alloc);
 }
 
-pub fn turnSafeDial(turns: []i64) i64 {
+pub fn findPassword(turns: []i64) i64 {
     var zeros: i64 = 0;
-    var position: i64 = 50;
+    var dial: i64 = START;
 
     for (turns) |t| {
-        //std.debug.print("Turn {} Position {}\n", .{ t, position });
-        position = @mod(position + t, MAX + 1); // to be 0 - 99 inclusive
-        if (position == 0) {
+        dial = @mod(dial + t, MAX_INC); // to be 0 - 99 inclusive
+        if (dial == 0) {
             zeros += 1;
         }
     }
@@ -59,15 +60,17 @@ pub fn turnSafeDial(turns: []i64) i64 {
     return zeros;
 }
 
-pub fn turnSafeDialMethod0x434C49434B(turns: []i64) i64 {
+pub fn findPasswordMethod0x434C49434B(turns: []i64) i64 {
     var zeros: i64 = 0;
-    var position: i64 = 50;
+    var dial: i64 = START;
 
     for (turns) |t| {
+        const steps: usize = @intCast(@abs(t));
         const delta: i64 = if (t > 0) 1 else -1;
-        for (0..@intCast(@abs(t))) |_| {
-            position = @mod(position + delta, MAX + 1);
-            if (position == 0) {
+
+        for (0..steps) |_| {
+            dial = @mod(dial + delta, MAX_INC);
+            if (dial == 0) {
                 zeros += 1;
             }
         }
@@ -80,14 +83,14 @@ pub fn part1(alloc: std.mem.Allocator, lines: []const []const u8) !i64 {
     const turns = try parse(alloc, lines);
     defer alloc.free(turns);
 
-    return turnSafeDial(turns);
+    return findPassword(turns);
 }
 
 pub fn part2(alloc: std.mem.Allocator, lines: []const []const u8) !i64 {
     const turns = try parse(alloc, lines);
     defer alloc.free(turns);
 
-    return turnSafeDialMethod0x434C49434B(turns);
+    return findPasswordMethod0x434C49434B(turns);
 }
 
 test "day01 part 1 example" {
